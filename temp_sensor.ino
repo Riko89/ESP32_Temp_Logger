81f81972586947b785085f4b224226497f964967
Eric Davey <39813738+Riko89@users.noreply.github.com>
2023-08-18 14:47:59 -0700

    Add files via upload

---
+++ temp_sensor.ino
@@ -0,0 +1,68 @@
+//Include required libraries
+#include "WiFi.h"
+#include <HTTPClient.h>
+#include <DHT.h>
+
+#define DHT_SENSOR_PIN  21 // ESP32 pin GPIO21 connected to DHT11 sensor
+#define DHT_SENSOR_TYPE DHT11
+
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+// WiFi credentials
+const char* ssid = "mango1";  // Enter SSID here
+const char* password = "freemango4u"; //password here
+// Google script ID and required credentials
+String GOOGLE_SCRIPT_ID = "AKfycbyaNLxim-lAIIFrixsy7dlQp1wTaCBHyt9-1Pqg-JvRUma6IpJ-zNttcEBIa3WDMrps";    // change Gscript ID
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  delay(1000);
+  dht_sensor.begin(); // initialize the DHT sensor
+  delay(1000);
+  // connect to WiFi
+  Serial.println();
+  Serial.print("Connecting to wifi: ");
+  Serial.println(ssid);
+  Serial.flush();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Init and get the time
+}
+void loop() {
+   if (WiFi.status() == WL_CONNECTED) {
+    static bool flag = false;
+        // read humidity
+    float humi = dht_sensor.readHumidity();
+    // read temperature in Celsius
+    float tempC = dht_sensor.readTemperature();
+    // read temperature in Fahrenheit
+    float tempF = dht_sensor.readTemperature(true);
+   //start WIFI
+    if ( isnan(tempC) || isnan(tempF) || isnan(humi)) {
+      Serial.println("Failed to read from DHT sensor!");
+    } else {
+      String urlFinal = "https://script.google.com/macros/s/"+GOOGLE_SCRIPT_ID+"/exec?"+"temperature=" + tempF + "&humidity=" + humi;
+      Serial.print("POST data to spreadsheet:");
+      Serial.println(urlFinal);
+      HTTPClient http;
+      http.begin(urlFinal.c_str());
+      http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+      int httpCode = http.GET(); 
+      Serial.print("HTTP Status Code: ");
+      Serial.println(httpCode);
+      //---------------------------------------------------------------------
+      //getting response from google sheet
+      String payload;
+      if (httpCode > 0) {
+          payload = http.getString();
+          Serial.println("Payload: "+payload);    
+    }
+    //---------------------------------------------------------------------
+    http.end();
+    }
+  }
+  delay(900000);
+} 
